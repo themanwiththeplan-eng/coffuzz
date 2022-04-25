2b94d16acddca85fe32b41ced30009e53a71b65b
themanwiththeplan-eng <dakotadunn5@gmail.com>
2022-04-24 17:44:02 -0700

    getting ready to add other types of requests for more functionality

--- cofuzz.py
+++ cofuzz.py
@@ -8,18 +8,19 @@ import argparse
 
 url = sys.argv[1]
 params = open(sys.argv[2], "r")
+requesttype = sys.argv[3]
 
 for p in params:
     stripped_line = p.strip()
-    httpurl = url + '/' + stripped_line
-    r = requests.get(httpurl, allow_redirects=True)
-    status = r.status_code
-    reason = r.reason
-    if status >= 200 and status <= 299:
-      print(r.url + ' --> ' + str(status))
-    elif status >= 300 and status <= 399:
-      print(r.url + ' --> ' + str(status))
-    elif status >= 400 and status <= 499:
-      print(r.url + ' --> ' + str(status))
-    
+    httpurl = url + '/' + stripped_line 
+    if requesttype == "GET":
+      r = requests.get(httpurl, allow_redirects=True)
+      status = r.status_code
+      reason = r.reason
+      if status >= 200 and status <= 299:
+        print(r.url + " --> " + str(status) + " " + reason)
+      elif status >= 300 and status <= 399:
+        print(r.history + " --> " + r.url + str(status) + " " + reason)
+      elif status >= 400 and status <= 499:
+        print(r.url + " --> " + str(status) + " " + reason)
 requestfromfile()
